e5d0ced6eba648a74f606cfdbe843b2c0202178a
Julien Blanchon <julien@blanchon.cc>
2024-12-06 12:42:23 +0100

    First commit Julien

---
+++ bak/benchscofi/PMF.py
@@ -0,0 +1,28 @@
+from scipy.sparse import csr_matrix
+from stanscofi.models import BasicModel
+
+from .implementations import BayesianPairwiseRanking
+
+
+# ' Matrix factorization
+class PMF(BasicModel):
+    def __init__(self, params=None):
+        params = params if (params is not None) else self.default_parameters()
+        super(PMF, self).__init__(params)
+        self.name = "PMF"
+        self.estimator = BayesianPairwiseRanking.BPR(**params)
+
+    def default_parameters(self):
+        params = BayesianPairwiseRanking.bpr_params
+        return params
+
+    def preprocessing(self, dataset, is_training=True):
+        # Use 0-1 ratings (matrix form)
+        return [csr_matrix(dataset.ratings.toarray().T)]
+
+    def model_fit(self, X):
+        self.estimator.fit(X)
+
+    def model_predict_proba(self, X):
+        preds = self.estimator.predict().T
+        return preds
